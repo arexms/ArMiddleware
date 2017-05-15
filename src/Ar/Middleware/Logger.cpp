f3c7a825f887dd2e28d30b381506de235bfaaf93
arexms <arex.ms@gmail.com>
2017-05-15 21:14:55 +0200

    Compilation bug under clang - no std::put_time

--- src/Ar/Middleware/Logger.cpp
+++ src/Ar/Middleware/Logger.cpp
@@ -75,7 +75,11 @@ namespace Ar { namespace Middleware
                 auto mc = mc_since_epoch.count() % 1000000;
 
             std::stringstream ss;
+#if __cplusplus >= 201103L      // does not wotk with older iomanip header - no std::put_time      
             ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X") << ":" << mc;
+#else
+            ss << "0000-00-00" << ":" << mc;
+#endif // __cplusplus >= 201103L
             std::string now = ss.str();
 
         std::string	temp = std::string( "[" ) + now + "][" + LogLevelStr[ _level ] + "][" + _prefix + "]" + " " + _prologue.prologue + str + "\n";
